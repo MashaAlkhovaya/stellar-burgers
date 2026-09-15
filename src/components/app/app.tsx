8baeeef11e5703b422255fa42139930d761a9993
МарияАльховая <MashaAlkhovaya@yandex.ru>
2026-09-15 20:22:05 +0300

    feat: добавлены модальные маршруты для ингридиентов и заказов

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,10 @@
-import { AppHeader, ProtectedRoute } from '@components';
+import {
+  AppHeader,
+  ProtectedRoute,
+  Modal,
+  IngredientDetails,
+  OrderInfo,
+} from '@components';
 import {
   ConstructorPage,
   Feed,
@@ -11,11 +17,18 @@ import {
   NotFound404,
 } from '@pages';
 import { Preloader } from '@ui';
-import { Routes, Route } from 'react-router-dom';
+import {
+  Routes,
+  Route,
+  useLocation,
+  useNavigate,
+  type Location,
+} from 'react-router-dom';
 
 import type { AppContentProps } from './type';
 
 import '../../index.css';
+
 import styles from './app.module.css';
 
 const App = (): React.JSX.Element => {
@@ -52,19 +65,123 @@ const AppContent = ({ isLoading, error }: AppContentProps): React.JSX.Element =>
 };
 
 const RouteComponent = (): React.JSX.Element => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const background = (location.state as { background?: Location })?.background;
   return (
     <>
-      <Routes>
+      <Routes location={background ?? location}>
         <Route path="/" element={<ConstructorPage />} />
         <Route path="/feed" element={<Feed />} />
-        <Route path="/login" element={<ProtectedRoute onlyUnAuth><Login /></ProtectedRoute>} />
-        <Route path="/register" element={<ProtectedRoute onlyUnAuth><Register /></ProtectedRoute>} />
-        <Route path="/forgot-password" element={<ProtectedRoute onlyUnAuth><ForgotPassword /></ProtectedRoute>} />
-        <Route path="/reset-password" element={<ProtectedRoute onlyUnAuth><ResetPassword /></ProtectedRoute>} />
-        <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-        <Route path="/profile/orders" element={<ProtectedRoute><ProfileOrders /></ProtectedRoute>} />
+        <Route
+          path="/login"
+          element={
+            <ProtectedRoute onlyUnAuth>
+              <Login />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <ProtectedRoute onlyUnAuth>
+              <Register />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/forgot-password"
+          element={
+            <ProtectedRoute onlyUnAuth>
+              <ForgotPassword />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/reset-password"
+          element={
+            <ProtectedRoute onlyUnAuth>
+              <ResetPassword />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/profile/orders"
+          element={
+            <ProtectedRoute>
+              <ProfileOrders />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/feed/:number"
+          element={
+            <Modal title="Детали заказа" onClose={() => void navigate('/')}>
+              <OrderInfo />
+            </Modal>
+          }
+        />
+        <Route
+          path="/ingredients/:id"
+          element={
+            <Modal title="Детали ингредиента" onClose={() => void navigate('/')}>
+              <IngredientDetails />
+            </Modal>
+          }
+        />
+        <Route
+          path="/profile/orders/:number"
+          element={
+            <ProtectedRoute>
+              <Modal
+                title="Детали заказа"
+                onClose={() => void navigate('/profile/orders')}
+              >
+                <OrderInfo />
+              </Modal>
+            </ProtectedRoute>
+          }
+        />
         <Route path="*" element={<NotFound404 />} />
       </Routes>
+      {background && (
+        <Routes>
+          <Route
+            path="/feed/:number"
+            element={
+              <Modal title="Детали заказа" onClose={() => void navigate(-1)}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+          <Route
+            path="/ingredients/:id"
+            element={
+              <Modal title="Детали ингредиента" onClose={() => void navigate(-1)}>
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+          <Route
+            path="/profile/orders/:number"
+            element={
+              <ProtectedRoute>
+                <Modal title="Детали заказа" onClose={() => void navigate(-1)}>
+                  <OrderInfo />
+                </Modal>
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      )}
     </>
   );
 };
--- src/components/protected-route/protected-route.tsx
+++ src/components/protected-route/protected-route.tsx
@@ -1,14 +1,14 @@
+import { Navigate } from 'react-router-dom';
+
 import type { ProtectedRouteProps } from './type';
-import {Navigate} from 'react-router-dom';
 
 export const ProtectedRoute = ({
   children,
   onlyUnAuth,
 }: ProtectedRouteProps): React.JSX.Element => {
-
   const isAuthenticated = false;
 
-  if(!onlyUnAuth && !isAuthenticated) {
+  if (!onlyUnAuth && !isAuthenticated) {
     return <Navigate to="/login" replace />;
   }
   if (onlyUnAuth && isAuthenticated) {
