0717999419a9e4c7fa5feed9a0c0ffb24ec6f4ad
МарияАльховая <MashaAlkhovaya@yandex.ru>
2026-09-16 10:59:33 +0300

    feat: feat: выведены данные ингридиентов в конструкторе бургера

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -1,3 +1,4 @@
+import { useSelector } from '@services';
 import { BurgerIngredientsUI } from '@ui';
 import { useMemo, useState, useRef, useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
@@ -9,8 +10,7 @@ export const BurgerIngredients = (): React.JSX.Element => {
   const titleBunRef = useRef<HTMLHeadingElement>(null);
   const titleMainRef = useRef<HTMLHeadingElement>(null);
   const titleSaucesRef = useRef<HTMLHeadingElement>(null);
-  // TODO: Взять ингредиенты из стора
-  const ingredients: TIngredient[] = [];
+  const ingredients = useSelector((state) => state.ingredients.ingredients);
 
   const [bunsRef, inViewBuns] = useInView({
     threshold: 0,
