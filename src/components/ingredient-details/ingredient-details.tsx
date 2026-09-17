aec973ee849c2d4e5f2f7ccb6d970ce10cf89082
МарияАльховая <MashaAlkhovaya@yandex.ru>
2026-09-17 15:41:27 +0300

    fix: слдержимое ингридиентов

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,8 +1,13 @@
+import { selectIngredients } from '@selectors';
+import { useSelector } from '@services';
 import { Preloader, IngredientDetailsUI } from '@ui';
+import { useParams } from 'react-router-dom';
 
 export const IngredientDetails = (): React.JSX.Element => {
-  // TODO: Взять переменную из стора
-  const ingredientData = null;
+  const { id } = useParams();
+  const ingredients = useSelector(selectIngredients);
+
+  const ingredientData = ingredients.find((item) => item._id === id);
 
   if (!ingredientData) {
     return <Preloader />;
